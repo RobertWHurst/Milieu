934319caa53520fb31456e5b218bcc6093b67475
Robert Hurst <robertwhurst@gmail.com>
2019-06-25 22:22:22 -0700

    typescript: Fix explain return type for nested val

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,34 +1,40 @@
 declare namespace milieu {
-
   export interface Opts {
-    argv?: string
-    env?: { [s: string]: string }
-    platform?: string
-    cwd?: string
-    unsetEnvValues?: boolean
-    parseValues?: boolean
+    argv?: string;
+    env?: { [s: string]: string };
+    platform?: string;
+    cwd?: string;
+    unsetEnvValues?: boolean;
+    parseValues?: boolean;
   }
+
+  export type Explanation<Config> = {
+    [Key in keyof Config]: Config[Key] extends object
+      ? Explanation<Config[Key]>
+      : { val: Config[Key]; src: string }
+  };
 
   export class MilieuConstructor<Config extends object> {
-    constructor (applicationName: string, defaults: Config, opts?: Opts)
-    explain (): { [K in keyof Config]: { val: Config[K], src: string } }
-    printExplainTable (): void
-    toObject (): Config
-    toJSON (): Config
+    constructor(applicationName: string, defaults: Partial<Config>, opts?: Opts);
+    explain(): Explanation<Config>;
+    printExplainTable(): void;
+    toObject(): Config;
+    toJSON(): Config;
   }
 
-  export type Milieu<Config extends object> = Config & MilieuConstructor<Config>
+  export type Milieu<Config extends object> = Config &
+    MilieuConstructor<Config>;
   export const Milieu: new <Config extends object>(
     applicationName: string,
-    defaults: Config,
+    defaults: Partial<Config>,
     opts?: Opts
-  ) => Milieu<Config>
+  ) => Milieu<Config>;
 }
 
 declare function milieu<Config extends object>(
   applicationName: string,
-  defaults: Config,
+  defaults: Partial<Config>,
   opts?: milieu.Opts
-): milieu.Milieu<Config>
+): milieu.Milieu<Config>;
 
-export = milieu
+export = milieu;
